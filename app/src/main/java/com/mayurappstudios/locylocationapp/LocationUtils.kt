43446f28ef4ea643d4ace44eb482b6aa86a2e941
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-01 16:04:28 +0530

    added a function for location check in LocationUtils.kt

--- app/src/main/java/com/mayurappstudios/locylocationapp/LocationUtils.kt
+++ app/src/main/java/com/mayurappstudios/locylocationapp/LocationUtils.kt
@@ -1,7 +1,21 @@
 package com.mayurappstudios.locylocationapp
 
 import android.content.Context
+import androidx.core.content.ContextCompat
 
-class LocationUtils(val context : Context) {
-
+class LocationUtils(val context: Context) {
+    fun hasLocationPermission(context: Context): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        } else {
+            return false
+        }
+    }
 }

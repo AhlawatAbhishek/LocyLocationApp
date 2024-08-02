4f47f4fe5efd71d3cb9396f6872c48e137179eca
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-08-02 17:23:56 +0530

    was working on LocationDisplay composable in MainActivity

--- app/src/main/java/com/mayurappstudios/locylocationapp/LocationUtils.kt
+++ app/src/main/java/com/mayurappstudios/locylocationapp/LocationUtils.kt
@@ -1,21 +1,19 @@
 package com.mayurappstudios.locylocationapp
 
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
 class LocationUtils(val context: Context) {
     fun hasLocationPermission(context: Context): Boolean {
-        if (ContextCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) == android.content.pm.PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                context,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        } else {
-            return false
-        }
+        return (ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                )
     }
+
 }
--- app/src/main/java/com/mayurappstudios/locylocationapp/MainActivity.kt
+++ app/src/main/java/com/mayurappstudios/locylocationapp/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.mayurappstudios.locylocationapp
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mayurappstudios.locylocationapp.ui.theme.LocyLocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +27,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocyLocationAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LocationDisplay(modifier = Modifier.padding(innerPadding), LocationUtils(this), this)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocyLocationAppTheme {
-        Greeting("Android")
+fun LocationDisplay(modifier: Modifier = Modifier, locationUtils: LocationUtils, context: Context) {
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { permissions ->
+                if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                    //Permission granted, update the location
+                } else {
+                    //Permission denied
+                }
+            })
+    Column(
+        modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Location not available")
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                //Permission already granted, update the location
+            } else {
+                //Request location permission
+
+            }
+        }) {
+            Text("Get Location")
+        }
     }
 }
